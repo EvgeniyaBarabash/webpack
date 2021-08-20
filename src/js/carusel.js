fe510dd79b43aef0f869678118fcec2d367a702b
EvgeniyaBarabash <barabash1989@gmail.com>
2021-08-20 18:35:38 +0300

    deploy

--- src/js/carusel.js
+++ src/js/carusel.js
@@ -1,34 +1,34 @@
-import refs from './refs.js';
-const { modalImg } = refs;
-import photoArray from '../db/photo.js';
+// import refs from './refs.js';
+// const { modalImg } = refs;
+// import photoArray from '../db/photo.js';
 
-export function carousel() {
-  window.addEventListener('keydown', (e) => {
-    if (e.code === 'ArrowLeft') {
-      arrowLeft();
-    }
-    if (e.code === 'ArrowRight') {
-      arrowRight();
-    }
-  });
-}
-function setNewSrc(step, index) {
-  modalImg.dataset.index = `${index + step}`;
-  modalImg.src = photoArray[index + step].image;
-}
-function arrowRight() {
-  let index = +modalImg.dataset.index;
-  if (index === photoArray.length - 1) {
-    setNewSrc(0, 0);
-    return;
-  }
-  setNewSrc(1, index);
-}
-function arrowLeft() {
-  let index = +modalImg.dataset.index;
-  if (index === 0) {
-    setNewSrc(0, photoArray.length - 1);
-    return;
-  }
-  setNewSrc(-1, index);
-}
+// export function carousel() {
+//   window.addEventListener('keydown', (e) => {
+//     if (e.code === 'ArrowLeft') {
+//       arrowLeft();
+//     }
+//     if (e.code === 'ArrowRight') {
+//       arrowRight();
+//     }
+//   });
+// }
+// function setNewSrc(step, index) {
+//   modalImg.dataset.index = `${index + step}`;
+//   modalImg.src = photoArray[index + step].image;
+// }
+// function arrowRight() {
+//   let index = +modalImg.dataset.index;
+//   if (index === photoArray.length - 1) {
+//     setNewSrc(0, 0);
+//     return;
+//   }
+//   setNewSrc(1, index);
+// }
+// function arrowLeft() {
+//   let index = +modalImg.dataset.index;
+//   if (index === 0) {
+//     setNewSrc(0, photoArray.length - 1);
+//     return;
+//   }
+//   setNewSrc(-1, index);
+// }
